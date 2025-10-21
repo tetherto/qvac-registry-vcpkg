c6ef830006148163a5b194d202c99f9de9d7bd6b
Sero <sero@Seros-MacBook-Pro.local>
2025-10-21 15:19:20 +0400

    addon-cpp version 0.12.1

--- ports/qvac-lib-inference-addon-cpp/portfile.cmake
+++ ports/qvac-lib-inference-addon-cpp/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH
   URL git@github.com:tetherto/qvac-lib-inference-addon-cpp.git
-  REF fb58407278128e06459ebcc5f33d65e74db20123
+  REF e4eb50f3e6f9ffe6f7a5cbbc29e827e8164beff4
 )
 
 vcpkg_check_features(
