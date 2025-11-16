6d22c544a377c17a76eed16a4cb78281de0ea635
mekhakt <mekhak.shoyan@tether.io>
2025-11-16 16:58:48 +0400

    Patch

--- ports/onnxruntime/portfile.cmake
+++ ports/onnxruntime/portfile.cmake
@@ -8,6 +8,7 @@ set(ONNXRUNTIME_PATCHES
   "04-fix-android-binary-size.patch"
   "05-add-dependencies-to-config.patch"
   "06-fix-array-bounds-issue-ios-sim.patch"
+  "09-fix-minimal-build-onnx-onnx-proto-issue.patch"
 )
 
 # Add extra patches only for Windows builds
