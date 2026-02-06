91138dd698e6293980a4337b6fd279577f6736a5
mekhakt <mekhak.shoyan@tether.io>
2026-02-06 09:42:44 +0400

    Fix review comment

--- ports/tokenizers-cpp/portfile.cmake
+++ ports/tokenizers-cpp/portfile.cmake
@@ -1,9 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mlc-ai/tokenizers-cpp
-    REF ae3d2062d4e08c04bdd9bdbcc59eed504463fcca
+    REF tags/v${VERSION}
     SHA512 bfe313afefdb9fb2876febe81ef1e5c46a6f3a691c047df87567711b03502e490af8b1decd427483d4496aaf8b46472358dd85b01463a88b6565cc93afdc712e
-    HEAD_REF tags/v0.1.1
     PATCHES
         0001-build-only-hf-tokenizer.patch
         0002-fix-rust-build.patch
